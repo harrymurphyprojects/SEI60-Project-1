7b948e0abe473ef4ced6a32e47acc3f68a8a4bd0
harrymurphy1534 <harryjmurphy1997@gmail.com>
2021-11-17 10:13:09 +0000

    Road function complete

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,13 @@ const width = 10
 const gridCellCount = width * width
 const startButton = document.querySelector('#start')
 console.log(startButton)
-// const laneOnePosition = generateLaneOne()
 
 const grid = document.querySelector('.grid')
 const cells = []
-let laneOnePosition = 62
+let laneOnePosition = 69
+let laneTwoPosition = 70
+let laneThreePosition = 89
 let frogPosition = 4
-
-// const carSpawn = ['80', '71', '62']
-// const laneOneCells = []
 
 // Grid Building 
 function createGrid() {
@@ -23,20 +21,34 @@ function createGrid() {
     grid.appendChild(cell)
   }
 }
-// console.log('before creating', cells)
 createGrid()
-// console.log(cells)
-// cells[50].classList.add('frog')
 
 // functions
+// RoadWay
 function removeLaneOne() {
-  cells[laneOnePosition].classList.Remove('carLaneOne')
+  cells[laneOnePosition].classList.remove('carLaneOne')
+}
+
+function removeLaneTwo() {
+  cells[laneTwoPosition].classList.remove('carLaneTwo')
+}
+
+function removeLaneThree() {
+  cells[laneThreePosition].classList.remove('carLaneThree')
 }
 
 function addLaneOne() {
   cells[laneOnePosition].classList.add('carLaneOne')
 }
 
+function addLaneTwo() {
+  cells[laneTwoPosition].classList.add('carLaneTwo')
+}
+
+function addLaneThree() {
+  cells[laneThreePosition].classList.add('carLaneThree')
+}
+// Frog 
 function removeFrog() {
   cells[frogPosition].classList.remove('frog')
 }
@@ -45,19 +57,13 @@ function addFrog() {
   cells[frogPosition].classList.add('frog')
 }
 
-// function generateLaneOne() {
-//   return cells[laneOnePosition]
-// }
-
 function handleStart() {
-  window.setInterval(() => {
-    // const laneOnePosition = generateLaneOne()
-    // removeLaneOne()
-    // generateLaneOne()
-  }, 1000)
   addLaneOne()
   addFrog()
   handleCarLaneOne()
+  handleCarLaneTwo()
+  handleCarLaneThree()
+  collisonCar()
 }
 
 function handleKeyUp(e) {
@@ -92,17 +98,81 @@ function handleKeyUp(e) {
 }
 
 function handleCarLaneOne() {
-  window.setInterval(() => {
-    if (laneOnePosition >= 54) {
-      console.log('hi')
-      removeLaneOne()
-      cells[laneOnePosition] = cells[laneOnePosition - 1]
-      addLaneOne()
+  window.setInterval(() => { 
+    removeLaneOne()
+    laneOnePosition -= 1
+    if (laneOnePosition === 59) {
+      laneOnePosition = laneOnePosition + width
     }
-  }, 1000)
-  // handleCarLaneOne()
+    collisonCar()
+    addLaneOne()
+    return
+  }, 200)
 }
 
+function handleCarLaneTwo() {
+  window.setInterval(() => { 
+    removeLaneTwo()
+    laneTwoPosition += 1
+    if (laneTwoPosition === 80) {
+      laneTwoPosition = laneTwoPosition - width 
+    } 
+    addLaneTwo()
+    return
+  }, 650)
+}
+
+function handleCarLaneThree() {
+  window.setInterval(() => { 
+    removeLaneThree()
+    laneThreePosition -= 1
+    if (laneThreePosition === 79) {
+      laneThreePosition = laneThreePosition + width
+    } 
+    addLaneThree()
+    return
+  }, 500)
+}
+
+function collisonCar() {
+  if (laneOnePosition === frogPosition) {
+    console.log('game over')
+    removeFrog() 
+    frogPosition = 4
+    addFrog()
+  }
+  if (laneTwoPosition === frogPosition) {
+    console.log('game over')
+    removeFrog()
+    frogPosition = 4
+    addFrog()
+  }
+  if (laneThreePosition === frogPosition) {
+    console.log('game over')
+    removeFrog() 
+    frogPosition = 4
+    addFrog()
+  }
+}
+//  ==========RIVER===========
+let logOnePosition = 19
+let logTwoPosition = 29
+let logThreePosition = 39
+
+function addLogOne() {
+  cells[logOnePosition].classList.add('logLaneOne')
+}
+
+function handleLogRiverOne() {
+  window.setInterval(() => { 
+    removeLogOne()
+    logOnePosition -= 1
+    if (logOnePosition === 9) {
+      logOnePosition = logOnePosition + width
+    }
+    addLogOne()
+    return
+  }, 500)
 // Event Listeners 
 
 startButton.addEventListener('click', handleStart)
@@ -116,71 +186,3 @@ document.addEventListener('keyup', handleKeyUp)
 
 
 
-// // * Dom Elements
-// const grid = document.querySelector('.grid')
-// const cells = []
-
-// // * grid variables
-// const width = 10
-// const cellCount = width * width
-
-// // * game variables
-// let froggerPosition = 0
-
-// // * grid building
-// function createGrid() {
-//   for (let i = 0; i < cellCount; i++) {
-//     const cell = document.createElement('div')
-//     cell.textContent = i
-//     grid.appendChild(cell)
-//     cells.push(cell)
-//   }
-//   addFrogger()
-// }
-
-// createGrid()
-
-// // * functions
-// function addFrogger() {
-//   cells[froggerPosition].classList.add('pika')
-// }
-
-// function removeFrogger() {
-//   cells[froggerPosition].classList.remove('pika')
-// }
-
-// function handleKeyUp(e) {
-//   console.log(e.code)
-//   const x = frogPosition % width
-//   const y = Math.floor(froggerPosition / width)
-
-//   removeFrogger()
-//   switch (e.code) {
-//     case 'ArrowRight':
-//       if (x < width - 1) {
-//         frogPosition++
-//       }
-//       break
-//     case 'ArrowLeft':
-//       if (x > 0) {
-//         frogPosition--
-//       }
-//       break
-//     case 'ArrowDown':
-//       if (y < width - 1) {
-//         frogPosition += width
-//       }
-//       break
-//     case 'ArrowUp':
-//       if (y > 0) {
-//         frogPosition -= width
-//       }
-//       break
-//     default:
-//       console.log('Invalid Key, do nothing')
-//   }
-//   addFrogger()
-// }
-
-// // * events
-// document.addEventListener('keyup', handleKeyUp)
